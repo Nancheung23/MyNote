1a6975c353802c5053209bdceb7bdd747e421519
Yannan Zhang <zhangyannan199744@outlook.com>
2024-10-29 21:41:20 +0200

    10.29 add basic router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
+// import {react} from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <App />,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>
 )
